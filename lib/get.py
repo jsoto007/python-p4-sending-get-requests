2fa070849e188eb7ef8c123c1ac4923345d827d6
jsoto007 <jsoto7087@gmail.com>
2025-03-23 17:00:47 -0400

    fetches JSON data

--- lib/get.py
+++ lib/get.py
@@ -1,7 +1,9 @@
 import requests
 import json
 
+# url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
 url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
+
 
 response = requests.get(url)
 
